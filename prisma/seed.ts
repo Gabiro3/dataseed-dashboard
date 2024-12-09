e651d830474cce63333a158d5bbe78ffe545cda4
gabiroarnaud <gabiroarnaud@gmail.com>
2024-12-09 08:01:24 +0000

    Initial commit

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { Day, PrismaClient, UserSex } from "@prisma/client";
+const { Day, PrismaClient, UserSex } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
